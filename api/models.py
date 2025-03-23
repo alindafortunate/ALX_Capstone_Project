b458f950795a6bafb61ac2c16c71c4f63ed8bb19
alindafortunate <alindafortunate5@gmail.com>
2025-03-23 20:26:40 +0300

    Added the models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Book(models.Model):
+    title=models.CharField(max_length=255)
+    author=models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
+    ISBN=models.CharField(max_length=255, unique=True)
+    number_of_copies_available=models.IntegerField()
+    
+
+
+class Transcations(models.Model):
+    pass
